cf13b342c8c2eca672c6d54e5f5a7eb0c524caac
Yeabsira Ashenafi <ashenafiyeabsira@gmail.com>
2024-04-08 17:12:47 +0300

    added capability to connect with wallet address

--- Frontend/web3-front/src/components/HelloWorld.vue
+++ Frontend/web3-front/src/components/HelloWorld.vue
@@ -1,5 +1,47 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button v-if="!isConnected" type="button" @click="connectWallet()">Connect to wallet</button>
+    <div v-else> The connected wallet address is {{ connectedAddress }}</div>
+  </div>
+
+</template>
+
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
+// uses the ethers project package , Provides functionalities to interact with the Ethereum blockchain using Ethers.js.
+import { Web3Provider } from '@ethersproject/providers';
+
+const isConnected = ref(false);
+const connectedAddress = ref(null);
+
+const connectWallet = async() => {
+  const { ethereum } = window;
+  if (!ethereum) {
+    console.error("MetaMask not detected");
+    return;
+  }
+
+  console.log("Connecting to wallet");
+  
+  // opens meta mask on the browser
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  
+  // A Web3Provider wraps a standard Web3 provider, which is
+  // what MetaMask injects as window.ethereum into each page
+  const provider = new Web3Provider(ethereum);
+
+  // gets the address that is connected
+  const signer = provider.getSigner();
+  const address = await signer.getAddress()
+
+  // You can now use the provider and signer objects to interact with the blockchain
+  console.log("Connected! Address:", address);
+  
+  isConnected.value = true;
+  connectedAddress.value = address;
+}
 
 defineProps({
   msg: String,
@@ -8,30 +50,6 @@ defineProps({
 const count = ref(0)
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-</template>
 
 <style scoped>
 .read-the-docs {
