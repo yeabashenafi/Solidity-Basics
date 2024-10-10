53713a44096307dd6e297e5cf300c34d4fa126f3
yeabashenafi <ashenafiyeabsira@gmail.com>
2024-10-10 21:59:53 +0300

    added deposit and Withdrawals module

---
+++ Ethereum Course/Deposit and Withdrawals/payable.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.15;
+
+contract SampleContract {
+
+    string public myString = "Hello World";
+
+    function update(string memory _newString) public payable{
+        // msg.value gets the ether value on the contract
+        // 1 ether is a common key word 
+        if (msg.value == 1 ether){
+            myString = _newString;
+        }
+        else{
+            // msg.sender is the address that deployed the contract
+            // transfer is a function that sends the specific amount
+            // payable type casts the message sender to a payable type
+            payable (msg.sender).transfer(msg.value);
+        }
+    }
+}
