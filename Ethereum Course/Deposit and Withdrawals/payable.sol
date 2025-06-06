688479860631c4da8e2e32514660636df5e23c40
yeabashenafi <ashenafiyeabsira@gmail.com>
2025-06-06 21:12:49 +0300

    improved comments

--- Ethereum Course/Deposit and Withdrawals/payable.sol
+++ Ethereum Course/Deposit and Withdrawals/payable.sol
@@ -6,11 +6,12 @@ contract SampleContract {
 
     string public myString = "Hello World";
 
-    // required to make the contract able to receive eth during 
+    // required to make the contract able to receive eth during deployment
     // constructor () payable {
 
     // }
 
+    // since the function is payable values can be passed
     function update(string memory _newString) public payable{
         // msg.value gets the ether value on the contract
         // 1 ether is a common key word 
@@ -18,10 +19,11 @@ contract SampleContract {
             myString = _newString;
         }
         else{
+            // sends the ether back to the sender 
             // msg.sender is the address that deployed the contract
             // transfer is a function that sends the specific amount
             // payable type casts the message sender to a payable type
             payable (msg.sender).transfer(msg.value);
         }
-    }
+    } 
 }
