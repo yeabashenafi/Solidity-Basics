90562353516356405158cad0074fc732a875900d
yeabashenafi <ashenafiyeabsira@gmail.com>
2025-06-22 18:41:15 +0300

    added comments

--- Ethereum Course/Data Structures,Exceptions&LowLevelCalls/mappingWithdrawal.sol
+++ Ethereum Course/Data Structures,Exceptions&LowLevelCalls/mappingWithdrawal.sol
@@ -2,20 +2,25 @@
 pragma solidity 0.8.15;
 
 contract ExampleMappingWithdrawal{
-    
+    // key value pair where the key is an ethereum cinema
     mapping(address => uint) public balanceReceived;
 
+    // function that can receive eth
     function sendMoney() public payable {
         balanceReceived[msg.sender] += msg.value;
     }
 
+    // returns the balance of the contract itself
     function getBalance() public view returns(uint){
         return  address(this).balance;
     }
 
     function withdrawAllMoney(address payable _to) public {
+        // gets the balance for the sender address inside the balance Received variable
         uint balanceToSendOut = balanceReceived[msg.sender];
+        // sets the msg senders value in the mapping to 0
         balanceReceived[msg.sender] = 0;
+        // actually sends the amount to the address in the parameter
         _to.transfer(balanceToSendOut);
     }
 }
