0cd3427818a2407e982832d7301a3cbc2742cdc0
yeabashenafi <ashenafiyeabsira@gmail.com>
2025-06-25 20:21:46 +0300

    added comments

--- Ethereum Course/Data Structures,Exceptions&LowLevelCalls/exceptionRequire.sol
+++ Ethereum Course/Data Structures,Exceptions&LowLevelCalls/exceptionRequire.sol
@@ -1,18 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.7.0;
+pragma solidity 0.8.15;
 
+// shows how to use the require statement
 contract ExampleExceptionRequire{
 
+    // tracks the balances received 
     mapping(address => uint8) public balanceReceived;
 
+    // ether receiving function
     function receiveMoney() public payable {
         // different from require in that it is used when the case must not be reached
         // and does not have a message
+        // used to validate conditions that logically should never be used
+        // if the assert fails the transaction will revert, all remaining gas will be consumed
+        // assert failures are considered critical internal errors
         assert(msg.value == uint8(msg.value));
         balanceReceived[msg.sender] += uint8(msg.value);
     }
 
-    function withdrawMoney(address payable _to, uint8 _amount ) public {
+    // the withdraw function WHICH HODLDS THE REQUIRE STATEMENT
+    // REQUIRE IS USED TO VALIDATE EXTERNAL CONDITIONS, INPUTS ABD STATE CHANGES
+    // transaction is reverted and gas is returned to the sender
+    function withdrawMoney(address payable _to, uint8 _amount ) public payable {
         require(_amount <= balanceReceived[msg.sender], "Reverting ! Not enough balance");
         balanceReceived[msg.sender] -= _amount;
         _to.transfer(_amount);
