1dbe4c918f13b1eaa9e7638d7e10883bc2c01ea7
yeabashenafi <ashenafiyeabsira@gmail.com>
2025-06-27 15:37:53 +0300

    added comments

--- Ethereum Course/Data Structures,Exceptions&LowLevelCalls/contractCall.sol
+++ Ethereum Course/Data Structures,Exceptions&LowLevelCalls/contractCall.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
+// shows different ways where contracts can interact
 
+
+// the recipient
+// saves the transaction related data
 contract ContractOne{
 
     mapping(address => uint) public addressBalance;
@@ -15,20 +19,38 @@ contract ContractOne{
 }
 
 // send and transfer methods usually use 2300 gas
+// the interactor contract
 contract ContractTwo{
     receive() external payable {}
-
-    function depositOnContractOne(address _contractOne) public {
-        // this way the contract can be called with referencing the contract 
-        // ContractOne one = ContractOne(_contractOne);
-        // one.deposit{value:10, gas:100000}(); 
+    
+    // function expected an address of a deployed contract one contract
+    function depositOnContractOne(address payable _contractOne) public {
+        // method one
+        // this way the contract can be called with referencing the contract
+        // recommended and type safe way
+        // creates an instance of contract one 
+        ContractOne one = ContractOne(_contractOne);
+        one.deposit{value:10, gas:100000}();
+        
+        // method two
         // we can also call the function using specifc call data by using the following method
+        // low level call which provide high flexibility but requires careful handling
+        // used to interact with contracts whose interfaces arent fully known at compile time
+        // abi.encodeWithSignature creates the bytecode neccessary to call the deposit function
         bytes memory payload = abi.encodeWithSignature("depoist()");
+        
+        // executes the above function to send ether to contract_one 
+        // the maximum gas 
         // values returned are success and the returned value from the function call
-        // if there is no function call the secind param can be empty 
-        // (bool success,)=_contractOne.call{value:10, gas: 1000000}(payload);
-        // we can just send the transactionn if the other contract has a receive function
-        (bool success, ) = _contractOne.call{value: 10, gas:3000000}("");
-        require(success);
+        // if there is no function return value the second param can be empty 
+        (bool success,) = _contractOne.call{value:10, gas: 100000}(payload);
+        require(success, "The above contract call failed");
+
+        // method three
+        // calling no specific function 
+        // we can just send the transactionn if the other contract has a receive function or fallback too if receive is not defined
+        (bool completed, ) = _contractOne.call{value: 10, gas:100000}("");
+        // reverts if the contract call is not successful
+        require(completed);
     }
 }
--- Ethereum Course/Data Structures,Exceptions&LowLevelCalls/tryCatch.sol
+++ Ethereum Course/Data Structures,Exceptions&LowLevelCalls/tryCatch.sol
@@ -1,23 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
+// contract is designed to cause a transaction to revert or fail
 contract WillThrow{
     // custom error definition
+    // are more gas efficient and help to provide specific error messages
     error NotAllowedError(string);
     function aFunction() public pure {
         // require(false, "Error Message");
         // assert(false);
-
         revert NotAllowedError("You are not allowed!!");
     }
 }
 
+// demonstrates how to call another contract and gracefully handle different types of errors that might be thrown by that external call
 contract ErrorHandling {
+    // These are events which are ways to log information on the blockchain that can be effieciently listened by the off-chain applications
+    // Each event is designed to log a different type of message in the try catch block
     event ErrorLogging(string reason);
     event ErrorLogCode(uint code);
     event ErrorLogBytes(bytes lowLevelData);
 
+
     function catchTheError() public {
+        // creates a new instance of the WillThrow contract
         WillThrow will = new WillThrow();
         try will.aFunction(){
             // try code that works in this case
@@ -25,7 +31,7 @@ contract ErrorHandling {
          catch Error(string memory reason){
             emit ErrorLogging(reason);
         } 
-        // fired from assert stat<
+        // fired from assert statement
         catch Panic(uint errorCode){
             emit ErrorLogCode(errorCode);
         } 
